8c0f80df0e29b20df2d6edbf2398d1d511a7051a
ClementRollin <clemrollin01@gmail.com>
2025-09-24 13:53:05 +0200

    made dashboard homepage

---
+++ resources/views/Filament/Pages/Setting.blade.php
@@ -0,0 +1,98 @@
+<x-filamet-panels::pages>
+  <section>
+    <div class="flex items-center justify-between">
+      <div>
+        <h1 class="">Bienvenu sur CRHOM</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+        <p><a href="lien"></a></p>
+
+      </div>
+      <div>
+        <img src="" alt="">
+      </div>
+    </div>
+
+  </section>
+
+  <section>
+    <div class="space-y-12" >
+      <div class="flex items-center justify-between gap-8">
+        <img src="" alt="" class="w- h- object-cover">
+      </div>
+      <div>
+        <h3 class="font-bold"></h3>
+        <p class="text-black-600">
+          Lorem ipsum dolor sit ame.
+        </p>
+      </div>
+    </div>
+
+    <div class="flex flex-row-reverse items-center justify-between gap-8">
+      <img src="" alt="" class="w- h- object-cover">
+      <div>
+        <h3 class="font-bold"></h3>
+        <p class="text-black-600">
+          Lorem ipsum dolor sit amet.
+        </p>
+      </div>
+
+      <div class="flex items-center justify-between gap-8">
+        <img src="" alt="">
+        <div>
+          <h3 class="font-bold"></h3>
+          <p class="text-black-600">
+            Lorem ipsum dolor sit, amet.
+          </p>
+        </div>
+      </div>
+
+      <div class="border-t border-black-400 mt-10 mb-10"></div>
+
+      <h2>Values of the club</h2>
+      <section class="py-12 bg-white text center">
+        <h2>Values of club</h2>
+
+        <div class="grid grid-cols-1 md:grid-cols3 gap-8 max-w-5xl mx-auto">
+          <div>
+            <h3 class="font-semibold mb-2">titre</h3>
+            <p>jvbsnvbsnvosvnbsjnvn</p>
+          </div>
+
+          <div>
+            <h3 class="font-semibold mb-2">titre</h3>
+            <p>jvbsnvbsnvosvnbsjnvn</p>
+          </div>
+
+          <div>
+            <h3 class="font-semibold mb-2">titre</h3>
+            <p>jvbsnvbsnvosvnbsjnvn</p>
+          </div>
+
+
+        </div>
+
+  </section>
+
+
+    </section>
+    <section class="py- bg-black-800 text-white">
+      <div class="max-w-6xl mx-auto px-4">
+        <h2 class="text-2xl font-bold mb-4">Office présentation</h2>
+        <p class="text-black-300 mb-12">
+          jvihsvugsèsugayucvi
+        </p>
+
+        <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols5 gap-8 text-center"></div>
+        <h3 class="font-semibold">Nom</h3>
+        <p class="text-black-400 text-sm text-sm">bbbfbfu</p>
+
+      </div>
+
+    </section>
+
+  </div>
+
+
+
+
+</x-filamet-panels::pages>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\LogoutController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/login', [AuthController::class, 'loginPost'])->name('login.post');
+Route::get('/register', [AuthController::class, 'register'])->name('register');
+Route::post('/register', [AuthController::class, 'registerPost'])->name('register.post');
