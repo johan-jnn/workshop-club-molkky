acc45355a7c1635b5e2b6d34e10ba630b90e5436
Alexis Marin <mail.alexismarin@gmail.com>
2025-09-24 15:24:03 +0200

    Added unit tests and the back-office link in the nav

---
+++ tests/Unit/UserFactoryTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class UserFactoryTest extends TestCase
+{
+    /**
+     * Test la génération d'un User par la factory.
+     */
+    public function test_user_factory_creates_valid_user(): void
+    {
+        $factory = new \Database\Factories\UserFactory();
+        $userData = $factory->definition();
+        $this->assertArrayHasKey('first_name', $userData);
+        $this->assertArrayHasKey('email', $userData);
+        $this->assertArrayHasKey('role', $userData);
+        $this->assertNotEmpty($userData['password']);
+    }
+}
---
+++ tests/Unit/UserReservationTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class UserReservationTest extends TestCase
+{
+    /**
+     * Test la relation reservations sur le modèle User.
+     */
+    public function test_user_has_reservations_relation(): void
+    {
+        $user = new \App\Models\User();
+        $this->assertTrue(method_exists($user, 'reservations'));
+    }
+}
